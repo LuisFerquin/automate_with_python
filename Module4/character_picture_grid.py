d77a94f1971666c8722d220711c97ae797064f4e
luisferquin <luisferquin@gmail.com>
2015-09-30 00:06:13 -0400

    Improve character picture grid

--- Module4/character_picture_grid.py
+++ Module4/character_picture_grid.py
@@ -9,9 +9,9 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
 def drawImage(list):
-    for j in range(6):
+    for j in range(len(grid(0))):
         for i in range(len(grid)):
             print(grid[i][j], end='')
-        print('')
+        print()
 
 drawImage(grid)
