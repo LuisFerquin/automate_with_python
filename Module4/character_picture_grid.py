6f64146a9f4a0bd17079bba2031d922ba18355b2
luisferquin <luisferquin@gmail.com>
2015-10-08 23:55:01 -0400

    Add more Module5 exercises, and update status

--- Module4/character_picture_grid.py
+++ Module4/character_picture_grid.py
@@ -9,7 +9,7 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
 def drawImage(list):
-    for j in range(len(grid(0))):
+    for j in range(len(grid[0])):
         for i in range(len(grid)):
             print(grid[i][j], end='')
         print()
